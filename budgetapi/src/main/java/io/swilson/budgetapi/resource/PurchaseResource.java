8392d98c15f1f077aeaebae8141a1228a7eefff6
Shelby Wilson <97818875+snw0502@users.noreply.github.com>
2024-06-27 20:47:06 -0400

    added getPurchases endpoint

--- budgetapi/src/main/java/io/swilson/budgetapi/resource/PurchaseResource.java
+++ budgetapi/src/main/java/io/swilson/budgetapi/resource/PurchaseResource.java
@@ -1,12 +1,36 @@
 package io.swilson.budgetapi.resource;
 
+import io.swilson.budgetapi.model.Response;
+import io.swilson.budgetapi.service.PurchaseService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static java.time.LocalDateTime.now;
+import static java.util.Map.*;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/purchases")
 @RequiredArgsConstructor
 public class PurchaseResource {
     // TODO: implement resource
+    private final PurchaseService purchaseService;
+
+    @GetMapping("/")
+    public ResponseEntity<Response> getPurchases() {
+        return ResponseEntity.ok(
+                Response.builder()
+                        .timeStamp(now())
+                        .data(of("purchases",purchaseService.list(50)))
+                        .message("Purchases retrieved.")
+                        .status(OK)
+                        .statusCode(OK.value())
+                        .build()
+        );
+    }
+
 }
